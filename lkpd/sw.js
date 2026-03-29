dbd7f2d41ba0d8da2baee96c479abaab831b324f
Pak Choyy <pulsachoy@gmail.com>
2026-03-29 18:40:24 +0700

    Refactor service worker for caching and fetching

--- lkpd/sw.js
+++ lkpd/sw.js
@@ -1,9 +1,32 @@
-self.addEventListener("install",e=>{
+const CACHE_NAME = "bantu-guru-v1";
 
-self.skipWaiting();
+const urlsToCache = [
+  "/lkpd/",
+  "/lkpd/index.html",
+  "/lkpd/manifest.json",
+  "/lkpd/guru-cibisd2.png"
+];
 
+// INSTALL
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch",e=>{
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
 
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
